de76215bf635664e1f16384f1f910cd3f10fda7a
Yee Yang <ahabion@gmail.com>
2014-12-10 15:36:29 -0500

    git init

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* Author: 
+  Yee Yang
+*/
+
+$(window).load(function() {
+    $('#slider').nivoSlider({
+      effect: 'random',
+      animSpeed: 1000,
+      pauseTime: 6000       
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
